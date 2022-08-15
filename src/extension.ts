11069cc379abc15e18d64b1fa824d505fab152ec
Stuart Boydell <s.boydell@paperlesswarehousing.com.au>
2022-08-15 15:30:33 +1000

    v0.0.1 Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "pickdate" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('pickdate.showdate', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		// get pick date and time
+		// SB 2022-08-03
+		const msPerDay = 86400000;
+		const pickEpoch = -63280800000; // Date('1967-12-31')[Symbol.toPrimitive]('number')
+		var localTime = new Date();
+
+		var pickMils = localTime[Symbol.toPrimitive]('number') - pickEpoch;
+		var pickDate = Math.floor(pickMils / msPerDay);
+		var pickTime = Math.floor((pickMils % msPerDay) / 1000);
+
+		vscode.env.clipboard.writeText(`${pickDate} ${pickTime}`);
+		vscode.window.showInformationMessage(`Pick date: ${pickDate}, time: ${pickTime} copied to clipboard`);
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
